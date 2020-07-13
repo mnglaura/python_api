e6aef3a0782cf9644f4e301f7a39758bdba83cf9
Laura <laura.s.mangu@gmail.com>
2020-07-13 21:31:01 +0300

    fixed data from HTML form not being read

--- server_api.py
+++ server_api.py
@@ -24,17 +24,26 @@ class Serv(BaseHTTPRequestHandler):
         self.wfile.write(bytes(file_to_open, 'utf-8'))
 
     def do_POST(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+        # self.send_response(200)
+        # self.send_header('Content-type', 'text/html')
+        # self.end_headers()
 
-        form = cgi.FieldStorage()
-        first_name= form.getvalue('fname')
-        last_name= form.getvalue('lname')
-        print("this is first name" + first_name)
+        # form = cgi.FieldStorage()
+        # first_name= form.getvalue('fname')
+        # last_name= form.getvalue('lname')
+        # print("this is first name" + first_name)
+
+        #get content length and use to retrieve post data
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        # print(post_data)
+        #post_data is bytes string, convert it to python string using decode
+        post_data = post_data.decode('utf-8')
+        #post_data is now fname=xxxxx&lname=xxxx, extract two vars from this string
+        post_vars = post_data.split('&')
+        first_name = post_vars[0].split('=')[1]
+        last_name = post_vars[1].split('=')[1]
         api.post_user_function(first_name, last_name)
-
-
 
 httpd = HTTPServer(('localhost', 8080), Serv)
 httpd.serve_forever()
